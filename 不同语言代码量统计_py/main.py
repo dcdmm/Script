3fb359023824e9b94e552cbc6430bab2b0a75ba4
duanmengmeng <dcdmmcomeon@gmail.com>
2025-12-10 16:46:23 +0800

    小的完善

--- 不同语言代码量统计_py/main.py
+++ 不同语言代码量统计_py/main.py
@@ -1,17 +1,39 @@
 import os
+import pathspec
+
+
+def load_gitignore(directory):
+    """加载目录下的.gitignore文件,返回pathspec对象"""
+    gitignore_path = os.path.join(directory, '.gitignore')
+    patterns = []
+    if os.path.exists(gitignore_path):
+        with open(gitignore_path, 'r', encoding='utf-8') as f:
+            patterns = f.read().splitlines()
+    patterns.append('.git/')  # 默认忽略.git目录
+    return pathspec.PathSpec.from_lines('gitwildmatch', patterns)
 
 
 def count_py_files(directory):
-    """递归统计给定目录下所有代码文件的数量"""
+    """递归统计给定目录下所有代码文件的数量(忽略.gitignore中的内容)"""
     code_cout = {".py": 0, ".ipynb": 0, ".sql": 0,
                  ".java": 0, ".scala": 0, ".cpp": 0,
                  ".h": 0, ".rs": 0, "CMakeLists.txt": 0}
+    spec = load_gitignore(directory)
     for root, dirs, files in os.walk(directory):
+        rel_root = os.path.relpath(root, directory)
+        if rel_root == '.':
+            rel_root = ''
+        dirs[:] = [d for d in dirs if not spec.match_file(
+            os.path.join(rel_root, d, '').replace('\\', '/') if rel_root else d + '/'
+        )]
         for file in files:
+            rel_file = os.path.join(rel_root, file).replace('\\', '/') if rel_root else file
+            if spec.match_file(rel_file):  # 跳过gitignore中指定的文件
+                continue
+
             for k in code_cout.keys():
                 if file.endswith(k):
                     code_cout[k] += 1
-
     return {key: value for key, value in code_cout.items() if value != 0}
 
 
